95aed311aa56fb16cc3a763de98bfa7572852849
Felipe Corrêa da Silva Sanches <juca@members.fsf.org>
2018-09-02 02:59:47 -0300

    nov e dez de 2017

--- associados_garoa.py
+++ associados_garoa.py
@@ -1,6 +1,7 @@
 Abdo = "Alexandre Abdo"
 Abreu = "Abreu" #nome?
 Alexandra = "Alexandra Percario"
+AllanTrindade = "Allan Trindade" # (de São Vicente)
 Anchises = "Anchises"
 Andre = "Andre" #nome completo? (seria esse o Hermann?)
 AndreHermann = "André Hermann"
@@ -9,12 +10,14 @@ Agaelebe = "Hugo Lima Borges"
 Aylons = "Gustavo Bruno"
 BrunoBorges = "Bruno Lima Borges" # (Irmão do Hugo)
 BrunpLP = "Bruno Luiz de Paula"
-CSM = "CSM" #nome?
+CSM = "CSM" #nome? Seria esse o Carlos CM?
+CarlosCM = "Carlos CM" # do time de CTF do Garoa
 Dandara = "Dandara Jatobá"
 Dente = "Dente" # nome?
 DQ = "Daniel Quadros"
-FabioH = "FabioHirano"
-Fabricio = "Fabricio" #nome?
+Erik = "Erik Dataleak" #nome real?
+FabioH = "Fabio Hirano"
+FabricioBiazzoto = "Fabricio Biazzotto"
 Fellype = "Fellype Cazorino"
 FMolina = "Fernando Molina"
 FSouza = "Felipe Souza"
@@ -23,11 +26,13 @@ GringoMexico = "Gringo do México" #nome real?
 Guisso = "Fernando Guisso"
 Gutem = "Gutemberg Nunes"
 GutoMaia = "Gustavo Maia Neto"
+Helena = "Helena (amiga do Afonso)"
 IanF = "Ian Fernandez"
 JAdriano = "João Adriano"
 JamesRaznor = "James Raznor"
+JamesSouza = "James Souza"
 Juca = "Felipe Correa da Silva Sanches"
-LaTeX = "LaTeX" #nome real?
+LaTeX = "Leandro Teixeira (LaTeX)"
 LAlcantara = "Lucas Alcântara"
 LuisLeao = "Luis Fernando de Oliveira Leão"
 Marcel = "Marcel" #nome?
@@ -40,9 +45,10 @@ NelsonBrito = "Nelson Brito"
 Oda = "Eduardo Oda"
 Pampolha = "Pampolha" #nome?
 Pitanga = "Rodrigo Rodrigues da Silva"
+RobertJr = "Robert Junior" # Guisso disse: "Ele é um cabeludinho, magrinho, com cara de nerd, que vem com o pai."
 RdosGatos = "Rodrigo dos Gatos" # nome real? Rodrigo Silveira talvez?
 RodrigoSilveira = "Rodrigo Silveira"
-Rubens = "Rubão" #verificar nome completo
+Rubens = "Rubão" #verificar nome completo (Rubens Tadeu talvez?)
 RMunhoz = "Rogério Munhoz"
 Sandro = "Sandro Friedland"
 Subnet = "Luís Guilherme Pires Martins de Abreu"
@@ -53,9 +59,26 @@ VAlves = "Vitor Alves"
 Vitor = "Vitor Fernandes"
 Vido = "Lucas Vido"
 VJPixel = "VJ Pixel"
-Vrech = "Matheus Vral Vrech"
+Vrech = "Matheus Vral Vrech" # (São Carlos)
 Yumi = "Amanda Yumi Ambriola"
 Wesley = "Wesley Shaimon"
+
+# CMC de 21 de Novembro de 2017:
+CMC.data("2017-11-21")
+Guisso.apresenta_padawans([Gabi,
+                           RobertJr])
+Afonse.apresenta_padawans([AllanTrindad,
+                           JamesSouza,
+                           MateusVrech,
+                           CarlosCM])
+
+# CMC de 19 de Dezembro de 2017:
+CMC.data("2017-12-19")
+Guisso.apresenta_padawans([RogerioMunhoz,
+                           IanF])
+CMC.aprova_associado(Mesel, endosso=[Vido, Yumi])
+CMC.aprova_associado(AllanTrindade, endosso=[Vido, Afonso])
+CMC.aprova_associado(JamesSouza, endosso=[Afonso])
 
 # CMC de 16 de Janeiro de 2018:
 CMC.data("2018-01-16")
