08ba6d3f3f3c3e99b61dd91797837680b21c48a0
Felipe Corrêa da Silva Sanches <juca@members.fsf.org>
2018-09-02 01:41:17 -0300

    rascunhando uma idéia. Comecando pela ata do CMC de 21/08/2018:
    https://garoa.net.br/wiki/Reunião_de_21/08/2018

---
+++ associados_garoa.py
@@ -0,0 +1,27 @@
+Alexandra = "Alexandra Percario"
+Anchises = "Anchises"
+Agaelebe = "Hugo Lima Borges"
+Dandara = "Dandara Jatobá"
+DQ = "Daniel Quadros"
+FSouza = "Felipe Souza"
+Gabrielzinho = "Gabriel Almeida"
+Gutem = "Gutemberg Nunes"
+Guisso = "Fernando Guisso"
+JAdriano = "João Adriano"
+Juca = "Felipe Correa da Silva Sanches"
+Mesel = "Vinicius Mesel"
+Oda = "Eduardo Oda"
+Pitanga = "Rodrigo Rodrigues da Silva"
+Sandro = "Sandro Friedland"
+VAlves = "Vitor Alves"
+Vitor = "Vitor Fernandes"
+Vido = "Lucas Vido"
+Yumi = "Amanda Yumi Ambriola"
+Wesley = "Wesley Shaimon"
+
+Afonso.apresenta_padawan("21/08/2018",
+                         [Alexandra,
+                          Dandara,
+                          FSouza,
+                          Wesley,
+                          VAlves])
