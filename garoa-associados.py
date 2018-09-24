2acabf7afc291a98d35586bcaf6ea8544406548b
Felipe Corrêa da Silva Sanches <juca@members.fsf.org>
2018-09-24 00:39:19 -0300

    espaçamento

--- garoa-associados.py
+++ garoa-associados.py
@@ -205,9 +205,11 @@ CMC = ConselhoMandaChuva(cofundadores)
 CMC.data("2011-02-22")
 # são mencionados 12 fundadores e 4 membros, mas sem citar nomes...
 
+
 # CMC de 15 de Março de 2011:
 CMC.data("2011-03-15")
 # Nessa reunião não se fala explicitamente de novos associados, apesar de constarem nomes de participantes na reunião que não assinaram os papéis da fundação.
+
 
 # CMC de 19 de Abril de 2011:
 CMC.data("2011-04-19")
@@ -235,6 +237,7 @@ CMC.aprova_associado(Markun, endosso="HACK")
 CMC.data("2011-05-11")
 # Não houve alteração no quadro de associados/padawans.
 
+
 # CMC de 24 de Maio de 2011:
 CMC.data("2011-05-24")
 # Consta na ata que Bruno Borges (irmão do Agaelebe) optou por não ser membro efetivo.
@@ -243,6 +246,7 @@ CMC.data("2011-05-24")
 # "Lint comunicou que não quer ser mais membro nem fundador":
 # CMC.observa_desligamento(Lint, motivo="Solicitou desligamento dos quadros do Garoa")
 
+
 # CMC de 21 de Junho de 2011:
 CMC.data("2011-06-21")
 Oda.apresenta_padawan(Samir)
@@ -250,6 +254,7 @@ Anchises.apresenta_padawan(CleitonAlves)
 # Em alguma data o Tony se associou (sem passar por padawanice?). Precisamos verificar.
 # Por enquanto vai constar aqui, pois é a primeira ata em que o nome dele é citado:
 CMC.aprova_associado(Tony, endosso="HACK")
+
 
 # CMC de 26 de Julho de 2011:
 CMC.data("2011-07-26")
@@ -260,11 +265,13 @@ Aleph.apresenta_padawan(Miklos)
 CMC.aprova_associado(RicardoAmaral, endosso="HACK")
 CMC.observa_desligamento(RicardoAmaral, motivo="3 (ou mais) meses de atraso na mensalidade")
 
+
 # CMC de 16 de Agosto de 2011:
 CMC.data("2011-08-16")
 CMC.aprova_associado(CleitonAlves) # "por quatro votos a favor e uma abstenção."
 Aylons.apresenta_padawans([Lechuga,
                            Diogenes])
+
 
 # CMC de 20 de Setembro de 2011:
 CMC.data("2011-09-20")
@@ -279,6 +286,7 @@ Anchises.apresenta_padawans([GabrielAraujo,
                              LuizVieira,
                              LoganBr])
 
+
 # CMC de 18 de Outubro de 2011:
 CMC.data("2011-10-18")
 Agaelebe.apresenta_padawan(WagnerSartori)
@@ -287,12 +295,14 @@ Tony.apresenta_padawan(AlexPorto)
 Oda.apresenta_padawan(Luca)
 CMC.aprova_associado(Carine)
 
+
 # CMC de 22 de Novembro de 2011:
 CMC.data("2011-11-22")
 Carine.apresenta_padawan(Nessa)
 CMC.aprova_associado(WagnerSartori, endosso=[Agaelebe])
 Anchises.apresenta_padawans([VictorScattone,
                              Coruja])
+
 
 # CMC de 13 de Dezembro de 2011:
 CMC.data("2011-12-13")
@@ -304,6 +314,7 @@ CMC.aprova_associado(LoganBr)
 CMC.aprova_associado(Nessa)
 CMC.aprova_associado(Coruja)
 
+
 # CMC de 17 de Janeiro de 2012:
 CMC.data("2012-01-17")
 Agaelebe.apresenta_padawan(Zanst)
@@ -312,6 +323,7 @@ WagnerSartori.apresenta_padawans([Leandro,
                                   Hugo])
 Aylons.apresenta_padawan(Gafanhoto)
 DQ.apresenta_padawan(Ulysses)
+
 
 # CMC de 28 de Fevereiro de 2012:
 CMC.data("2012-02-28")
@@ -322,14 +334,17 @@ CMC.aprova_associado(LuisLeao, endosso=[Lechuga])
 CMC.aprova_associado(Gafanhoto, endosso=[Aylons])
 CMC.aprova_associado(Luca, endosso=[Oda])
 
+
 # CMC de 20 de Março de 2012:
 CMC.data("2012-03-20")
 Aleph.apresenta_padawan(Murilo)
+
 
 # CMC de 17 de Abril de 2012:
 CMC.data("2012-04-17")
 Lechuga.apresenta_padawan(Vitor)
 CMC.aprova_associado(Ulysses, endosso=[DQ])
+
 
 # CMC de 22 de Maio de 2012:
 CMC.data("2012-05-22")
@@ -344,6 +359,7 @@ CMC.data("2012-06-19")
 # Não houve alteração no quadro de associados/padawans
 # Há referencia a 2 páginas de listagem de padawans e de associados, porem as páginas não existem mais.
 
+
 # CMC de 17 de Julho de 2012:
 CMC.data("2012-07-17")
 Aleph.apresenta_padawans([MauroAlex,
@@ -355,11 +371,13 @@ Ulysses.apresenta_padawans([RenatoCandido,
 CMC.aprova_associado(Tales, endosso=Aylons)
 CMC.observa_desligamento(Miklos, motivo="a pedido próprio em 4/julho/12")
 
+
 # CMC de 21 de Agosto de 2012:
 CMC.data("2012-08-21")
 Ramalho.apresenta_padawan(GutoMaia)
 Aylons.apresenta_padawan(ThiagoMassa)
 CMC.aprova_associado(LeandroSilva)
+
 
 # CMC de 18 de Setembro de 2012:
 CMC.data("2012-09-18")
@@ -369,9 +387,11 @@ CMC.aprova_associado(RicardoOliveira)
 # A ata diz: "2 pessoas não se manifestaram sobre resolver a situação e serão desligadas."
 # Quem são?!
 
+
 # CMC de 16 de Outubro de 2012:
 CMC.data("2012-10-16")
 # Não houve alteração do quadro de associados/padawans
+
 
 # CMC de 27 de Novembro de 2012:
 CMC.data("2012-11-27")
@@ -379,18 +399,22 @@ Oda.apresenta_padawan(Subnet)
 CMC.aprova_associado(Jonnes, endosso=[Aleph])
 CMC.aprova_associado(GutoMaia, endosso=[Ramalho])
 
+
 # CMC de 18 de Dezembro de 2012:
 CMC.data("2012-12-18")
 # Não houve alteração do quadro de associados/padawans
+
 
 # CMC de 15 de Janeiro de 2013:
 CMC.data("2013-01-15")
 CMC.aprova_associado(Subnet)
 
+
 # CMC de 19 de Fevereiro de 2013:
 CMC.data("2013-02-19")
 Ramalho.apresenta_padawan(Novelli)
 CMC.aprova_associado(Gola, endosso=[Tony])
+
 
 # CMC de 19 de Março de 2013:
 CMC.data("2013-03-19")
@@ -406,11 +430,13 @@ CMC.aprova_associado(Mike, endosso=[Agaelebe])
 VictorScattone.apresenta_padawan(G4b1s) #Gabriel Romano
 Juca.apresenta_padawan(ThiagoRondon)
 
+
 # CMC de 16 de Abril de 2013:
 CMC.data("2013-04-16")
 Tony.apresenta_padawan(Caique)
 Vitor.apresenta_padawan(Ceci)
 CMC.aprova_associado(G4b1s, endosso=[VictorScattone])
+
 
 # CMC de 21 de Maio de 2013:
 CMC.data("2013-05-21")
@@ -419,9 +445,11 @@ Aylons.apresenta_padawan(Moreno)
 CMC.aprova_associado(ThiagoRondon, endosso=[Juca])
 CMC.aprova_associado(Moreno, endosso=[Aylons])
 
+
 # CMC de 25 de Junho de 2013:
 CMC.data("2013-06-25")
 # Não houve alteração do quadro de associados/padawans
+
 
 # CMC de 16 de Julho de 2013:
 CMC.data("2013-07-16")
@@ -429,11 +457,13 @@ Vitor.apresenta_padawan(Ceci)
 LuisLeao.apresenta_padawan(Livia)
 Tony.apresenta_padawan(Liane)
 
+
 # CMC de 20 de Agosto de 2013:
 CMC.data("2013-08-20")
 Ramalho.apresenta_padawan(RafaelML)
 Ulysses.apresenta_padawan(Abayomi)
 CMC.aprova_associado(Ceci)
+
 
 # CMC de 17 de Setembro de 2013:
 CMC.data("2013-09-17")
@@ -443,14 +473,17 @@ CMC.aprova_associado(RafaelML, endosso=[Ramalho])
 CMC.observa_desligamento(Novelli, motivo="a pedido do próprio")
 CMC.observa_desligamento(Eden, motivo="por não ter pago nenhuma mensalidade")
 
+
 # CMC de 15 de Outubro de 2013:
 CMC.data("2013-10-15")
 # Não houve alteração do quadro de associados/padawans
+
 
 # CMC de 19 de Novembro de 2013:
 CMC.data("2013-11-19")
 CMC.aprova_associado(Kinoshita, endosso=[Juca])
 Oda.apresenta_padawan(Abdo)
+
 
 # CMC de 17 de Dezembro de 2013:
 CMC.data("2013-12-17")
@@ -460,6 +493,7 @@ CMC.aprova_associado(Abdo, endosso=[Oda])
 CMC.aprova_associado(Grigolato, endosso=[Ramalho])
 CMC.aprova_associado(AleSouza, endosso=[Ulysses])
 CMC.observa_desligamento(VictorScattone, motivo="temporariamente a pedido do mesmo")
+
 
 # CMC de 21 de Janeiro de 2014:
 CMC.data("2014-01-21")
@@ -474,6 +508,7 @@ MOTIVO="Sócios com mais de três meses de atraso na contribuição, automaticam
 CMC.observa_desligamento(Nessa, motivo=MOTIVO)
 CMC.observa_desligamento(Carine, motivo=MOTIVO)
 
+
 # CMC de 18 de Fevereiro de 2014:
 CMC.data("2014-02-18")
 #CMC.readmite_associado(Juca) # Na verdade isso só aconteceu no mês seguinte,
@@ -483,6 +518,7 @@ Juca.apresenta_padawans([Kemel,
 Vitor.apresenta_padawans([AndreOliveira,
                           Afonso])
 CMC.aprova_associado(Afonso, endosso=Vitor)
+
 
 # CMC de 18 de Março de 2014:
 CMC.data("2014-03-18")
@@ -494,6 +530,7 @@ CMC.aprova_associado(Kemel, endosso=[Ramalho,
                                      Juca,
                                      Haoji])
 
+
 # CMC de 15 de Abril de 2014:
 CMC.data("2014-04-15")
 CMC.aprova_associado(LeonardoTelles, endosso=Juca)
@@ -504,11 +541,13 @@ CMC.observa_desligamento(WagnerSartori, motivo=ATRASO)
 CMC.observa_desligamento(GutoMaia, motivo=ATRASO)
 CMC.observa_desligamento(Gola, motivo=ATRASO)
 
+
 # CMC de 20 de Maio de 2014:
 CMC.data("2014-05-20")
 Afonso.apresenta_padawans([Buzz,
                            Igor])
 CMC.readmite_associado(GutoMaia)
+
 
 # CMC de 18 de Junho de 2014:
 CMC.data("2014-06-18")
@@ -552,6 +591,7 @@ CMC.aprova_associado(JeanPaul, endosso=Juca)
 CMC.aprova_associado(Otto, endosso=Mike)
 CMC.aprova_associado(RogerRussel)
 
+
 # CMC de 21 de Outubro de 2014:
 CMC.data("2014-10-21")
 Juca.apresenta_padawan(EmersonMarques)
@@ -584,6 +624,7 @@ CMC.aprova_associado(Bilo, endosso=Oda)
 # CMC de 20 de Janeiro de 2015:
 CMC.data("2015-01-20")
 # Nenhuma alteração no quadro de associados e padawans
+
 
 # CMC de 10 de Fevereiro de 2015:
 CMC.data("2015-02-10")
@@ -702,6 +743,7 @@ Mike.apresenta_padawan(Carril)
 CMC.aprova_associado(DanielaIwassa, endosso=[Mike])
 CMC.aprova_associado(Carril, endosso=[Mike])
 
+
 # CMC de 16 de Fevereiro de 2016:
 CMC.data("2016-02-16")
 Ramalho.apresenta_padawan(Villares)
@@ -709,6 +751,7 @@ Mike.apresenta_padawan(MarceloCampos)
 CMC.aprova_associado(Villares, endosso=[Ramalho])
 CMC.aprova_associado(MarceloCampos, endosso=[Mike])
 # Pitanga apresentou uma proposta de "endorsements" que foi acatada ao Regimento Interno, mas nunca vi ser implementada de fato.
+
 
 # CMC de 15 de Março de 2016:
 CMC.data("2016-03-15")
@@ -719,13 +762,16 @@ CMC.aprova_associado(Gutem, endosso=[Mike])
 CMC.aprova_associado(JAFreitas, endosso=[Mike])
 CMC.aprova_associado(BrunoBorges, endosso=[Agaelebe])
 
+
 # CMC de 19 de Abril de 2016:
 CMC.data("2016-04-19")
 CMC.readmite_associado(Ceci) # "Se possível, Ceci solicita retomar a associação junto ao Garoa Hacker Clube, bem como retornar a ser membro do CMC e retomar a moderação das listas do Garoa, bem como de suas redes sociais. Saudadinhas, hehe...  ;)"
 
+
 # CMC de 17 de Maio de 2016:
 CMC.data("2016-05-17")
 # Não houve movimentação no quadro de associados/padawans
+
 
 # CMC de 21 de Junho de 2016:
 CMC.data("2016-06-21")
@@ -744,6 +790,7 @@ CMC.observa_desligamento(AndreOliveira, motivo="3 (ou mais) meses de atraso na m
 CMC.data("2016-07-19")
 CMC.aprova_associado(FabricioBiazzotto, endosso=[TiagoQueiroz])
 
+
 # CMC de 16 de Agosto de 2016:
 CMC.data("2016-08-16")
 FabioH.apresenta_padawan(Luiz) # ata diz apenas "Fabio"
@@ -752,6 +799,7 @@ CMC.readmite_associado(Jonnes) # DQ sugeriu. "Ele se afastou por algum tempo mas
 CMC.readmite_associado(Afonso) # "Afonso pede para reativar associação e entrar no Starving Hacker"
 CMC.observa_desligamento(Tales) # "Anchises informa que Tales Cione solicitou o seu desligamento"
 
+
 # CMC de 20 de Setembro de 2016:
 CMC.data("2016-09-20")
 Agaelebe.apresenta_padawan(Ivar)
@@ -759,6 +807,7 @@ CMC.observa_desligamento(Tony, motivo="3 (ou mais) meses de atraso na mensalidad
 CMC.pergunta("LaTeX foi desligado nessa ata, mas não consta em atas passadas quando foi que ele originalmente se tornou associado.")
 # CMC.observa_desligamento(LaTeX, motivo="3 (ou mais) meses de atraso na mensalidade") #dúvida: ata diz "Leandro"
 CMC.observa_desligamento(G4b1s, motivo="3 (ou mais) meses de atraso na mensalidade")
+
 
 # CMC de 18 de Outubro de 2016:
 CMC.data("2016-10-18")
@@ -773,12 +822,14 @@ CMC.pergunta("LeonardoSantos foi desligado nessa ata, mas não consta em atas pa
 CMC.data("2016-11-15")
 FabricioBiazzotto.apresenta_padawan(RodrigoSilveira)
 
+
 # CMC de 20 de Dezembro de 2016:
 CMC.data("2016-12-20")
 Anchises.apresenta_padawan(ErickEmiliano)
 Yumi.apresenta_padawan(Guisso) # implicitamente
 CMC.aprova_associado(Guisso, endosso=[Yumi])
 CMC.aprova_associado(RodrigoSilveira, endosso=[FabricioBiazzotto])
+
 
 # CMC de 17 de Janeiro de 2017:
 CMC.data("2017-01-17")
@@ -795,9 +846,11 @@ Ramalho.apresenta_padawan(Belasco)
 # CMC.observa_desligamento(RogerRussel,     motivo="3 (ou mais) meses de atraso na mensalidade")
 # CMC.observa_desligamento(AndreOliveira, motivo="3 (ou mais) meses de atraso na mensalidade")
 
+
 # CMC de 21 de Fevereiro de 2017:
 CMC.data("2017-02-21")
 # Nenhuma alteração no quadro de associados/padawans
+
 
 # CMC de 21 de Março de 2017:
 CMC.data("2017-03-21")
@@ -812,6 +865,7 @@ CMC.pergunta("A ata menciona desligamento de 'Alan'. Seria o Alan Moreira? Ou al
 # CMC.observa_desligamento(Allan, motivo="6 meses de atraso na mensalidade")
 # CMC.observa_desligamento(G4b1s, motivo="6 meses de atraso na mensalidade")
 
+
 # CMC de 18 de Abril de 2017:
 CMC.data("2017-04-18")
 CMC.pergunta("Uma pessoa com o nome (apelido?) 'Yanava' foi desligada nessa ata, mas não consta em atas passadas quando foi que originalmente se tornou associado(a).")
@@ -819,6 +873,7 @@ CMC.pergunta("Uma pessoa com o nome (apelido?) 'Yanava' foi desligada nessa ata,
 # CMC.observa_desligamento(LaTeX, motivo="3 meses de atraso na mensalidade") # ata diz "Leandro" (é o LaTeX mesmo?)
 CMC.observa_desligamento(DQ, motivo="requisitado")
 # NOTA: CMC aprova DQ como associado honorário.
+
 
 # CMC de 16 de Maio de 2017:
 CMC.data("2017-05-16")
@@ -830,6 +885,7 @@ Mike.apresenta_padawans([Dente,
 Oda.apresenta_padawan(Christian) # implicitamente
 CMC.aprova_associado(Christian, endosso=[Oda])
 
+
 # CMC de 20 de Junho de 2017:
 CMC.data("2017-06-20")
 CMC.aprova_associado(Dente)
@@ -837,11 +893,13 @@ CMC.aprova_associado(Anderson)
 CMC.aprova_associado(Thomas)
 # a ata diz: "Total de associados do Garoa no final de Abril: 40 (?)"
 
+
 # CMC de 18 de Julho de 2017:
 CMC.data("2017-07-18")
 CMC.readmite_associado(DQ)
 # CMC.aprova_associado(EmersonMonteiro, endosso=[Vido])
 CMC.pergunta("Porque o Vido propôs aprovar o Emerson Monteiro, que já tinha sido aprovado em Março de 2017?")
+
 
 # CMC de 15 de Agosto de 2017:
 CMC.data("2017-08-15")
@@ -849,6 +907,7 @@ Dente.apresenta_padawan(Taumaturgo) # "o cara da cerveja"
 Juca.apresenta_padawan(GustavoRibeiro)
 CMC.aprova_associado(GustavoRibeiro, endosso=[Juca])
 CMC.observa_desligamento(GutoMaia, motivo="3 meses de atraso na mensalidade")
+
 
 # CMC de 19 de Setembro de 2017:
 CMC.data("2017-09-19")
@@ -858,10 +917,12 @@ Oda.apresenta_padawan(Rubens) # implicitamente
 CMC.aprova_associado(Rubens, endosso=[Oda])
 CMC.observa_desligamento(Christian, motivo="3 meses de atraso na mensalidade")
 
+
 # CMC de 17 de Outubro de 2017:
 CMC.data("2017-10-17")
 # Nota: "Padawan do Dente (Taumaturgo) fugiu para a Espanha"
 # Não houve movimentações no quadro de associados / padawans
+
 
 # CMC de 21 de Novembro de 2017:
 CMC.data("2017-11-21")
@@ -872,6 +933,7 @@ Afonso.apresenta_padawans([AllanTrindade,
                            Vrech,
                            CarlosCM])
 
+
 # CMC de 19 de Dezembro de 2017:
 CMC.data("2017-12-19")
 Guisso.apresenta_padawans([RogerioMunhoz,
@@ -880,16 +942,19 @@ CMC.aprova_associado(Mesel, endosso=[Vido, Yumi])
 CMC.aprova_associado(AllanTrindade, endosso=[Vido, Afonso])
 CMC.aprova_associado(JamesSouza, endosso=[Afonso])
 
+
 # CMC de 16 de Janeiro de 2018:
 CMC.data("2018-01-16")
 Afonso.apresenta_padawan(JamesRaznor)
 Mike.apresenta_padawan(Marcel) # faz cerveja, lava panela de feijoada e faz pizza.
 Mesel.apresenta_padawan(NelsonBrito)
 
+
 # CMC de 20 de Fevereiro de 2018:
 CMC.data("2018-02-20")
 Guisso.apresenta_padawan(AndreHermann)
 CMC.readmite_associado(Ulysses)
+
 
 # CMC de 20 de Março de 2018:
 CMC.data("2018-03-20")
@@ -897,11 +962,13 @@ CMC.observa_desligamento(NelsonCanton) # solicitou
 CMC.observa_desligamento(Aylons) # solicitou
 CMC.aprova_associado(NelsonBrito, endosso=[Mesel])
 
+
 # CMC de 17 de Abril de 2018:
 CMC.data("2018-04-17")
 CMC.pergunta("'BrunoLP' foi desligado nesse CMC, mas não constava quando se tornou associado originalmente. Seriam 'BrunoLP' e 'BrunoDJ' a mesma pessoa?")
 # CMC.observa_desligamento(BrunoLP, motivo="3 meses de atraso")
 CMC.pergunta("Dúvida: Marcelo Rodrigues ainda é associado?")
+
 
 # CMC de 15 de Maio de 2018:
 CMC.data("2018-05-15")
@@ -913,6 +980,7 @@ CMC.observa_desligamento(Guisso) # (não requer autorização do CMC)
 #  já estão a 3 semanas tocando a atividade
 #  Clube de Estudos de Algoritmos."
 
+
 # CMC de 19 de Junho de 2018:
 CMC.data("2018-06-19")
 Juca.apresenta_padawan(LAlcantara)
@@ -920,10 +988,12 @@ CMC.aprova_associado(IanF, endosso=[Oda, Yumi])
 CMC.readmite_associado(Guisso) # mediante quitação de atraso de 6 mensalidades
 CMC.observa_desligamento(BrunoBorges)
 
+
 # CMC de 17 de Julho de 2018:
 CMC.data("2018-07-17")
 Afonso.apresenta_padawan(Vrech)
 CMC.aprova_associado(LAlcantara, endosso=[Juca])
+
 
 # CMC de 21 de Agosto de 2018:
 CMC.data("2018-08-21")
